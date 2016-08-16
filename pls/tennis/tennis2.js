791a2825199192f75d9ab6ba9c64229fc52f6967
cfloresASC <carlosaflores15@gmail.com>
2016-08-16 11:18:55 -0400

    e

--- pls/tennis/tennis2.js
+++ pls/tennis/tennis2.js
@@ -19,6 +19,9 @@ function setup() {
   textSize(70);
   textAlign(CENTER, CENTER);
 
+  back = createSprite(675, 300, 675, 300);
+  back.addImage(loadImage("https://cfloresasc.github.io/pls/tennis/tennis.jpg"))
+
   player = createSprite(30, height/2, 10, 100);
   player.immovable = true;
   player.addImage(loadImage("https://cfloresasc.github.io/pls/tennis/images.png")); 
