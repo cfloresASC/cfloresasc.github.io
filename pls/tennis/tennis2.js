92516ee8c831c260150402c194c79b36c7d81495
cfloresASC <carlosaflores15@gmail.com>
2016-08-16 12:38:57 -0400

    b

--- pls/tennis/tennis2.js
+++ pls/tennis/tennis2.js
@@ -18,9 +18,6 @@ function setup() {
   textSize(70);
   textAlign(CENTER, CENTER);
 
-  back = createSprite(675, 300, 675, 300);
-  back.addImage(loadImage("https://cfloresasc.github.io/pls/tennis/tennis.jpg"))
-
   player = createSprite(30, height/2, 10, 100);
   player.immovable = true;
   player.addImage(loadImage("https://cfloresasc.github.io/pls/tennis/images.png")) 
@@ -39,6 +36,9 @@ function setup() {
   ball.maxSpeed = MAX_SPEED1;
   ball.shapeColor = color(255,255,255);
   ball.setSpeed(MAX_SPEED, -180);
+
+    back = createSprite(675, 300, 675, 300);
+  back.addImage(loadImage("https://cfloresasc.github.io/pls/tennis/tennis.jpg"))
 }
 
 function draw() {
